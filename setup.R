deebe83113d28776668ed257c9ae4ec5b0ec394f
Mauro Lepore <maurolepore@gmail.com>
2024-01-18 18:39:00 +0000

    Rename hello.world to tiltRequirements

--- setup.R
+++ setup.R
@@ -1,11 +1,11 @@
 # https://ropensci.org/blog/2021/06/22/setup-runiverse/#the-packagesjson-registry-file
 tibble::tribble(
-              ~package,                                                      ~url,
-         "hello.world",        "https://github.com/2DegreesInvesting/hello.world",
-  "tilt.company.match", "https://github.com/2DegreesInvesting/tilt.company.match",
-         "tiltToyData",        "https://github.com/2DegreesInvesting/tiltToyData",
-       "tiltIndicator",      "https://github.com/2DegreesInvesting/tiltIndicator",
-  "tiltIndicatorAfter", "https://github.com/2DegreesInvesting/tiltIndicatorAfter",
-       "tiltWorkflows",      "https://github.com/2DegreesInvesting/tiltWorkflows",
+              ~package,                                                       ~url,
+    "tiltRequirements",    "https://github.com/2DegreesInvesting/tiltRequirements",
+  "tilt.company.match",  "https://github.com/2DegreesInvesting/tilt.company.match",
+         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",
+       "tiltIndicator",       "https://github.com/2DegreesInvesting/tiltIndicator",
+  "tiltIndicatorAfter",  "https://github.com/2DegreesInvesting/tiltIndicatorAfter",
+       "tiltWorkflows",       "https://github.com/2DegreesInvesting/tiltWorkflows",
 ) |>
   jsonlite::write_json('packages.json', pretty = TRUE)
