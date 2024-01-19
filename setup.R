725da2b90151e62bf7ac5693b80bc4e53b5d6c48
Mauro Lepore <maurolepore@gmail.com>
2024-01-19 21:02:00 +0000

    For tiltToyData use the released version from r-universe

--- setup.R
+++ setup.R
@@ -3,7 +3,7 @@ tibble::tribble(
               ~package,                                                       ~url,     ~branch,
     "tiltRequirements",    "https://github.com/2DegreesInvesting/tiltRequirements",  "*release",
   "tilt.company.match",  "https://github.com/2DegreesInvesting/tilt.company.match",      "main",
-         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",      "main",
+         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",  "*release",
        "tiltIndicator",       "https://github.com/2DegreesInvesting/tiltIndicator",  "*release",
   "tiltIndicatorAfter",  "https://github.com/2DegreesInvesting/tiltIndicatorAfter",      "main",
        "tiltWorkflows",       "https://github.com/2DegreesInvesting/tiltWorkflows",      "main",
