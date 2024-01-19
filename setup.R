b645831cd5bd4a21fbd866f97afc6fd85cb7e544
Mauro Lepore <maurolepore@gmail.com>
2024-01-19 17:24:00 +0000

    For tiltRequirements use the latest GitHub release

--- setup.R
+++ setup.R
@@ -1,11 +1,11 @@
 # https://ropensci.org/blog/2021/06/22/setup-runiverse/#the-packagesjson-registry-file
 tibble::tribble(
-              ~package,                                                       ~url,
-    "tiltRequirements",    "https://github.com/2DegreesInvesting/tiltRequirements",
-  "tilt.company.match",  "https://github.com/2DegreesInvesting/tilt.company.match",
-         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",
-       "tiltIndicator",       "https://github.com/2DegreesInvesting/tiltIndicator",
-  "tiltIndicatorAfter",  "https://github.com/2DegreesInvesting/tiltIndicatorAfter",
-       "tiltWorkflows",       "https://github.com/2DegreesInvesting/tiltWorkflows",
+              ~package,                                                       ~url,     ~branch,
+    "tiltRequirements",    "https://github.com/2DegreesInvesting/tiltRequirements",  "*release",
+  "tilt.company.match",  "https://github.com/2DegreesInvesting/tilt.company.match",      "main",
+         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",      "main",
+       "tiltIndicator",       "https://github.com/2DegreesInvesting/tiltIndicator",      "main",
+  "tiltIndicatorAfter",  "https://github.com/2DegreesInvesting/tiltIndicatorAfter",      "main",
+       "tiltWorkflows",       "https://github.com/2DegreesInvesting/tiltWorkflows",      "main",
 ) |>
   jsonlite::write_json('packages.json', pretty = TRUE)
