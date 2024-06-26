9fcdd153cc3e18dbe7205f6a40672122a7d9a2db
Mauro Lepore <maurolepore@gmail.com>
2024-06-26 17:48:46 -0300

    Update setup.R

--- setup.R
+++ setup.R
@@ -2,10 +2,5 @@
 tibble::tribble(
               ~package,                                                       ~url,     ~branch,
     "tiltRequirements",    "https://github.com/2DegreesInvesting/tiltRequirements",  "*release",
-  "tilt.company.match",  "https://github.com/2DegreesInvesting/tilt.company.match",      "main",
-         "tiltToyData",         "https://github.com/2DegreesInvesting/tiltToyData",  "*release",
-       "tiltIndicator",       "https://github.com/2DegreesInvesting/tiltIndicator",  "*release",
-  "tiltIndicatorAfter",  "https://github.com/2DegreesInvesting/tiltIndicatorAfter",      "main",
-       "tiltWorkflows",       "https://github.com/2DegreesInvesting/tiltWorkflows",      "main",
 ) |>
   jsonlite::write_json('packages.json', pretty = TRUE)
